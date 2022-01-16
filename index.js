a5b5685ba0835619155191699c31c3debf2c77f8
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-16 14:17:36 +0700

    add dom + check winner

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//Generate random number
 let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-// console.log(randomNumber1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+//Manipulate attribute
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+img1.src = `images/dice${randomNumber1}.png`;
+img2.src = `images/dice${randomNumber2}.png`;
+//Checking the winner
+const heading = document.querySelector(".container h1");
+if(randomNumber1 === randomNumber2){
+    heading.innerHTML = "Draw!";
+}else if(randomNumber1 > randomNumber2){
+    heading.innerHTML = "Player 1 Wins!";
+}else{
+    heading.innerHTML = "Player 2 Wins!";
+}
